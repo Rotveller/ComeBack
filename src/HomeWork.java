24896b609b53e29d665be2808551f9578a592f98
cahas <bulatovalexsandr1999@gmail.com>
2024-11-10 14:16:35 +0400

    First step

---
+++ src/HomeWork.java
@@ -0,0 +1,11 @@
+public class HomeWork {
+    public static void main (String[] args){
+        int a =114151515;
+        byte b = 12;
+        short c = 11415;
+        long d = 1151511516;
+        float f = 1.11f;
+        double g = 1.141151515f;
+        System.out.println(a);
+    }
+}
