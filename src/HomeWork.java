0cec8a5abe9a07ea635766e7ce813c19cba7d1b7
cahas <bulatovalexsandr1999@gmail.com>
2024-11-10 15:24:13 +0400

    2 step

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,6 +1,21 @@
 public class HomeWork {
-    public static void main (String[] args){
-        int a =114151515;
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        //task4();
+        //task5();
+        //task6();
+        //task7();
+        //task8();
+        //task9();
+        //task10();
+
+
+    }
+
+    public static void task1() {
+        int a = 114151515;
         byte b = 12;
         short c = 11415;
         long d = 1151511516;
@@ -8,4 +23,41 @@ public class HomeWork {
         double g = 1.141151515f;
         System.out.println(a);
     }
+
+    public static void task2() {
+        double dog = 8;
+        float cat = 3.6f;
+        int paper = 763789;
+        System.out.println(dog + "  " + cat + "  " + paper);
+        System.out.printf("===============");
+        System.out.println();
+        dog = dog + 2;
+        System.out.println(dog);
+        cat = cat + 2;
+        System.out.println(cat);
+        paper = paper + 2;
+        System.out.println(paper);
+        System.out.println();
+        System.out.printf("===============");
+        dog = dog - 3.5;
+        cat = cat - 1.6f;
+        paper = paper - 7639;
+        System.out.println();
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    }
+
+    public static void task3() {
+
+    }
+
+
 }
+
